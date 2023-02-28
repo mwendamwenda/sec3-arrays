742bfdc19b7bb4e86dd44edce055fc6ae1079338
mwendamwenda <benmwenda83@gmail.com>
2023-02-28 15:33:10 -0800

    dot notstion

--- index.js
+++ index.js
@@ -31,35 +31,76 @@
 // const ages = [calAge(years[0]),calAge(years[1]),calAge(years[years.length - 1])];
 // console.log(ages);
 
-const friends = ['brian','alex','peter'];
-//adds element
-friends.push('Jay');
-console.log(friends);
+// const friends = ['brian','alex','peter'];
+// //adds element
+// friends.push('Jay');
+// console.log(friends);
 
-friends.unshift('Allan');
-console.log(friends);
+// friends.unshift('Allan');
+// console.log(friends);
 
-//remove elements
-friends.pop();
-console.log(friends);
+// //remove elements
+// friends.pop();
+// console.log(friends);
 
-friends.shift();
-console.log(friends);
+// friends.shift();
+// console.log(friends);
 
-console.log(friends.indexOf('alex'));
+// console.log(friends.indexOf('alex'));
 
-console.log(friends.includes('alex'));
-console.log(friends.includes('mwaniki')); 
+// console.log(friends.includes('alex'));
+// console.log(friends.includes('mwaniki')); 
 
-if(friends.includes('alex')){
-    console.log('you have a freind called Alex');
-}
-// code challenge
+// if(friends.includes('alex')){
+//     console.log('you have a freind called Alex');
+// }
+// // code challenge
 
-const calTip = function(bill){
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
-}
+// const calTip = function(bill){
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+// }
 
-const bills = [125,555,44];
-const tip = [calTip(bills[0]),calTip(bills[1]),calTip(bills[2])];
-console.log(tip);
+// const bills = [125,555,44];
+// const tip = [calTip(bills[0]),calTip(bills[1]),calTip(bills[2])];
+// console.log(tip);
+
+//objects
+const mwendaArray = [
+    'Benson',
+    'mwenda',
+    2037 - 1992,                //array
+    'teacher',
+    ['ken','mbuguz','nic']
+];
+
+const Benson = {
+    firstName : 'Gichuru',
+    lastName : 'mwenda',
+    occupation : 'teacher',
+    age : 2037 - 1992,
+    friends : ['ken','mbuguz','nic']
+};
+console.log(Benson);
+
+console.log(Benson.lastName);//dot notation
+console.log(Benson['lastName']);
+
+const nameKey = 'Name';
+console.log(Benson['first' + nameKey]);
+console.log(Benson['last' + nameKey]);
+
+const interestedIn =  prompt('what do you want to know about benson? choose between firstName,lastName,age,job,and friends');
+console.log(Benson[interestedIn]);
+
+if(Benson[interestedIn]){
+    console.log(Benson[interestedIn]);
+}else{
+    console.log('wrong request! choose amomg the four');
+};
+
+Benson.location = 'nairobi';
+Benson['twitter'] = '@geektarage';
+
+console.log(Benson);
+
+console.log(`${Benson.firstName} has ${Benson.friends.length} friends ,and his best friend is called ${Benson.friends[0]}`);
